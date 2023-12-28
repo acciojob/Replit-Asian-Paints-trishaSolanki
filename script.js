7bdc7f03c8a15de8ddb0ef6035f2c588d1d3bc11
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-28 22:08:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+document.getElementById('change_button').addEventListener('click', function() {
+      const blockId = document.getElementById('block_id').value;
+      const color = document.getElementById('color_id').value;
+      const block = document.getElementById(blockId);
+
+      if (block) {
+        document.querySelectorAll('.grid-item').forEach(item => {
+          item.style.backgroundColor = 'transparent';
+        });
+        block.style.backgroundColor = color;
+      } else {
+        alert('Invalid block ID');
+      }
+    });
+
+    document.getElementById('reset_button').addEventListener('click', function() {
+      document.querySelectorAll('.grid-item').forEach(item => {
+        item.style.backgroundColor = 'transparent';
+      });
+    });
